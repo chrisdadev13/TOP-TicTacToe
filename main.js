b97863a35322b9873350f352ae757c6f2d5cd4d3
chrisdadev13 <chrisdadev13@gmail.com>
2022-01-31 18:20:23 -0400

    Check for winner (with some bugs)

--- main.js
+++ main.js
@@ -27,21 +27,93 @@ const gameBoard = (() => {
 
   //Get a winner based on array structure:
 
-  const winningAxes = [
-        [0,1,2],
+  /*const winningAxes = [
+        [0,1,2], //Rows
         [3,4,5],
         [6,7,8],
-        [0,3,6],
+        [0,3,6], // Columns
         [1,4,7],
-        [2,5,8],
-        [0,4,8],
+        [2,5,8], 
+        [0,4,8], // Diagonal
         [2,4,6],
-  ];
+  ];*/
   
-  const rowCheck = () => {
+  let winner = false;
 
+  let whoWin = [];
+
+  const firstDiagonal = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] == arr[i + 4] && arr[i + 4] == arr[i + 8]){
+        winner = true;
+        whoWin.push(arr[i], arr[i + 4], arr[i + 8]);
+      }
+      else{
+        continue;
+      }
+    }
+    if(winner == true && whoWin[0] == "X"){
+      alert(`We have a winner, congratulations player ${playerOne.name}`);
+    }
+    else if(winner == true && whoWin[0] == "O"){
+      alert(`We have a winner, congratulations player ${playerTwo.name}`);
+    }
   }
-  
+
+  const secondDiagonal = (arr) => {
+    for(let i = 2; i < arr.length; i++){
+      if(arr[i] == arr[i + 2] && arr[i + 2] == arr[i + 4]){
+        winner = true;
+        whoWin.push(arr[i], arr[i + 2], arr[i + 4]);
+      }
+      else{
+        continue;
+      }
+    }
+    if(winner == true && whoWin[0] == "X"){
+      alert(`We have a winner, congratulations player ${playerOne.name}`);
+    }
+    else if(winner == true && whoWin[0] == "O"){
+      alert(`We have a winner, congratulations player ${playerTwo.name}`);
+    }
+  }
+
+  const rowWinner = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] == arr[i + 1] && arr[i + 1] == arr[i + 2]){
+        winner = true;
+        whoWin.push(arr[i], arr[i + 1], arr[i + 2]);
+      }
+      else{
+        continue;
+      }
+     }
+    if(winner == true && whoWin[0] == "X"){
+      alert(`We have a winner, congratulations player ${playerOne.name}`);
+    }
+    else if(winner == true && whoWin[0] == "O"){
+      alert(`We have a winner, congratulations player ${playerTwo.name}`);
+    }
+  }
+
+  const columnWinner = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] == arr[i + 3] && arr[i + 3] == arr[i + 6]){
+        winner = true;
+        whoWin.push(arr[i], arr[i + 3], arr[i + 6]);
+      }
+      else{
+        continue;
+      }
+    }
+    if(winner == true && whoWin[0] == "X"){
+      alert(`We have a winner, congratulations player ${playerOne.name}`);
+    }
+    else if(winner == true && whoWin[0] == "O"){
+      alert(`We have a winner`)
+    }
+  }
+
   //Before put the respective player mark in the DOM, ask if the tile clicked is the same in the array, change the array and add the mark in DOM
   const displayController = (() => {
     tiles.forEach((tile) => {
@@ -49,7 +121,7 @@ const gameBoard = (() => {
         let div = document.createElement("div");
 
         for(let i = 0; i < boardArr.length; i++){
-          if(tile.getAttribute("id") == boardArr[i] && playerOne.turn == true){
+          if(tile.getAttribute("id") == boardArr[i] && playerOne.turn == true && winner == false){
             boardArr[i] = playerOne.sign;
             console.log(boardArr)
             
@@ -59,7 +131,7 @@ const gameBoard = (() => {
             playerOne.turn = false;
             playerTwo.turn = true;
           }
-          else if(tile.getAttribute("id") == boardArr[i] && playerOne.turn == false && playerTwo.turn == true){
+          else if(tile.getAttribute("id") == boardArr[i] && playerOne.turn == false && playerTwo.turn == true && winner == false){
             boardArr[i] = playerTwo.sign;
             console.log(boardArr);
   
@@ -70,9 +142,12 @@ const gameBoard = (() => {
             playerTwo.turn = false;
           }
         }
-  
-      })
-    })
-  })();
+        rowWinner(boardArr);
+        columnWinner(boardArr);
+        firstDiagonal(boardArr);
+        secondDiagonal(boardArr);
 
+        })
+      })
+    })();
 })();
