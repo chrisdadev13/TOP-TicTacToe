994b7b6ce021d8310943a631c483b08172d9f63d
chrisdadev13 <chrisdadev13@gmail.com>
2022-03-01 21:58:11 -0400

    Update: player name

--- main.js
+++ main.js
@@ -2,51 +2,104 @@ const start = (() => {
   const startContainer = document.querySelector(".start-container");
   const startButton = document.querySelector(".start-button");
   const boardContainer = document.querySelector(".board-container");
+  const playerNameContainer = document.querySelector("#playerName");
+  const playButton = document.querySelector("#press-to-play");
+  const playerOneInput = document.querySelector("#player-one-input"); 
+  const playerTwoInput = document.querySelector("#player-two-input");
 
   startButton.addEventListener("click", () => {
     startContainer.style.display = "none";
-    boardContainer.style.display = ""; 
+    playerNameContainer.style.display = "";
+    playerOneInput.value = "";
+    playerTwoInput.value = "";
+  });
+
+ playButton.addEventListener("click", () => {
+    if(playerOneInput.value != "" && playerTwoInput.value != ""){
+      playerNameContainer.style.display = "none";
+      boardContainer.style.display = "";
+    }
+    else{
+      playerOneInput.value = "Please introduce a valid name";
+    }
   });
 })();
 
-const gameBoard = (() => {
-  const Player = (name, sign, turn) => {
-    return {name, sign, turn};
+class Player{
+  constructor(name, mark, turn){
+    this.name = name;
+    this.mark = mark;
+    this.turn = turn;
   }
-  
-  let playerOne = Player("One", "X", true);
-  let playerTwo = Player("Two", "O", false);
 
-  let boardArr = [];
-  let oldArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];  
-  //Full the array with HTML tiles value so if player selection == tiles value, array change:
-  let tiles = document.querySelectorAll("div.board > div.play");
-  tiles.forEach((tile) => {
-    boardArr.push(tile.getAttribute("id"));
-  })
+  setName(name){
+    this.name = name;
+    return this;
+  }
 
-  //Get a winner based on array structure:
+  setMark(mark){
+    this.mark = mark;
+    return this;
+  }
 
-  /*const winningAxes = [
-        [0,1,2], //Rows
-        [3,4,5],
-        [6,7,8],
-        [0,3,6], // Columns
-        [1,4,7],
-        [2,5,8], 
-        [0,4,8], // Diagonal
-        [2,4,6],
-  ];*/
-  
-  let winner = false;
+  setTurn(turn){
+    this.turn = turn;
+    return this;
+  }
+}
 
-  let whoWin = [];
+class Main{
+  static TicTacToe(){
+    Main.game();
+  }
 
-  const firstDiagonal = (arr) => {
+  static getPlayerOneName(){
+    const input = document.querySelector("#player-one-input");
+    const name = input.value;
+    return name;
+  }
+
+  static getPlayerTwoName(){
+    const input = document.querySelector("#player-two-input");
+    const name = input.value;
+    return name;
+  }
+
+  static playerOne(){
+    let player = new Player;
+    player.setName(Main.getPlayerOneName());
+    player.setMark("X");
+    player.setTurn(true);
+
+
+    return player;
+  }
+
+  static playerTwo(){
+    let player = new Player;
+    player.setName(Main.getPlayerTwoName());
+    player.setMark("O");
+    player.setTurn(false);
+
+
+    return player;
+  }
+
+  static createBoardArr(){
+    let arr = [];
+    let boardDOM = document.querySelectorAll("div.board > div.play");
+    boardDOM.forEach((tile) => {
+      arr.push(tile.getAttribute("id"));
+    })
+    return arr;
+  }
+
+  static firstDiagonal(arr){
     for(let i = 0; i < arr.length; i++){
       if(arr[i] == arr[i + 4] && arr[i + 4] == arr[i + 8]){
         winner = true;
-        whoWin.push(arr[i], arr[i + 4], arr[i + 8]);
+        Main.whoWin(arr[i]);
+        Main.declareWinner();
       }
       else{
         continue;
@@ -54,43 +107,48 @@ const gameBoard = (() => {
     }
   }
 
-  const secondDiagonal = (arr) => {
+  static secondDiagonal(arr){
     for(let i = 0; i < arr.length; i++){
       if(arr[2] == arr[2 + 2] && arr[2 + 2] == arr[2 + 4]){
         winner = true;
-        whoWin.push(arr[2], arr[2 + 2], arr[2 + 4]);
+        Main.whoWin(arr[2]);
+        Main.declareWinner();
       }
       else{
         continue;
       }
-    }
+    }    
   }
 
-  const rowWinner = (arr) => {
+  static rowWinner(arr){
     for(let i = 0; i < arr.length; i++){
       if(arr[0] == arr[0 + 1] && arr[0 + 1] == arr[0 + 2]){
         winner = true;
-        whoWin.push(arr[0], arr[0 + 1], arr[0 + 2]);
+        Main.whoWin(arr[0]);
+        Main.declareWinner();
       }
       else if(arr[3] == arr[3 + 1] && arr[3 + 1] == arr[3 + 2]){
         winner = true;
-        whoWin.push(arr[3], arr[3 + 1], arr[3 + 2]);
+        Main.whoWin(arr[3]);
+        Main.declareWinner();
       }
       else if(arr[6] == arr[6 + 1] && arr[6 + 1] == arr[6 + 2]){
         winner = true;
-        whoWin.push(arr[6], arr[6 + 1], arr[6 + 2]);
+        Main.whoWin(arr[6]);
+        Main.declareWinner();
       }
       else{
         continue;
       }
-     }
+    }
   }
 
-  const columnWinner = (arr) => {
+  static columnWinner(arr){
     for(let i = 0; i < arr.length; i++){
       if(arr[i] == arr[i + 3] && arr[i + 3] == arr[i + 6]){
         winner = true;
-        whoWin.push(arr[i], arr[i + 3], arr[i + 6]);
+        Main.whoWin(arr[i]);
+        Main.declareWinner();
       }
       else{
         continue;
@@ -98,73 +156,76 @@ const gameBoard = (() => {
     }
   }
 
-  //Show winner and restart button
-  let modal = document.querySelector(".winner-container");
-  let restartBtn = document.querySelector("#press-to-restart");
-  
-  restartBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-    window.location.reload();
-  })
-
-  let winnerName = document.querySelector("#winner");
-
-  const declareWinner = () => {
-    if(winner == true && whoWin[0] == "X"){
-      modal.style.display = "";
-      winnerName.textContent = `Player: ${playerOne.name}`;
-    }
-    else if(winner == true && whoWin[0] == "O"){
-      modal.style.display = "";
-      winnerName.textContent = `Player: ${playerTwo.name}`;
-    }
-  } 
-  // Tie, check the elements in the array and if winner is false and the elements in every element in the array are "X" or "O"
-
-  const tie = (arr) => {
-    if(winner == false && arr.every(elem => elem == "X" || elem == "O")){
-      alert("Is a tie");
+  static tie(arr){
+    if(winner == false && arr.every(elem => elem == "X" || "O")){
+      alert("We have a tie");
     }
   }
 
-  //Before put the respective player mark in the DOM, ask if the tile clicked is the same in the array, change the array and add the mark in DOM
-  const displayController = (() => {
-    tiles.forEach((tile) => {
+  static game(){
+    let boardDOM = document.querySelectorAll("div.board > div.play");
+    let boardArr = Main.createBoardArr();
+    let playerOne = Main.playerOne();
+    document.querySelector("#player-one").textContent = playerOne.name + " X";
+    let playerTwo = Main.playerTwo();
+    document.querySelector("#player-two").textContent = playerTwo.name + " O";
+    let winner = false;
+
+    boardDOM.forEach((tile) => {
       tile.addEventListener("click", () => {
-        let div = document.createElement("div");
-
         for(let i = 0; i < boardArr.length; i++){
-          if(tile.getAttribute("id") == boardArr[i] && playerOne.turn == true && winner == false){
-            boardArr[i] = playerOne.sign;
-            console.log(boardArr)
-            
-            div.textContent = playerOne.sign;
-            tile.appendChild(div);
-
-            playerOne.turn = false;
-            playerTwo.turn = true;
-          }
-          else if(tile.getAttribute("id") == boardArr[i] && playerOne.turn == false && playerTwo.turn == true && winner == false){
-            boardArr[i] = playerTwo.sign;
+          if(tile.getAttribute("id") == boardArr[i] && playerOne.turn == true){
+            boardArr[i] = playerOne.mark;
             console.log(boardArr);
-  
-            div.textContent = playerTwo.sign;
-            div.style.color = "#d63d36";
-            tile.appendChild(div);
-
-            playerOne.turn = true;
-            playerTwo.turn = false;
+            tile.textContent = playerOne.mark;
+            playerOne.setTurn(false);
           }
+          else if(tile.getAttribute("id") == boardArr[i] && playerOne.turn == false){
+            boardArr[i] = playerTwo.mark;
+            console.log(boardArr);
+            tile.textContent = playerTwo.mark;
+            tile.style.color = "red";
+            playerOne.setTurn(true);
+          }
+          else if(winner == true){
+            alert("We have a winner");
+          }
+          Main.firstDiagonal(boardArr);
+          Main.secondDiagonal(boardArr);
+          Main.rowWinner(boardArr);
+          Main.columnWinner(boardArr);
+          Main.tie(boardArr);
         }
-
-        rowWinner(boardArr);
-        columnWinner(boardArr);
-        firstDiagonal(boardArr);
-        secondDiagonal(boardArr);
-        tie(boardArr);
-        declareWinner();
-        
-        })
       })
-    })();
-})();
+    })
+    return boardArr;
+  }
+
+  static restartGame(){
+    let restartBtn = document.querySelector("#press-to-restart");
+    restartBtn.addEventListener("click", () => {
+      window.location.reload();
+    })
+  }
+
+  static declareWinner(){
+    let modal = document.querySelector(".winner-container");
+
+    modal.style.display = "";
+    Main.restartGame();
+  }
+
+  static whoWin(mark){
+    let winnerDOM = document.querySelector("#winner");
+    let playerOne = Main.playerOne();
+    let playerTwo = Main.playerTwo();
+    if(mark == "X"){
+      winnerDOM.textContent = playerOne.name;
+    }
+    else if(mark == "O"){
+      winnerDOM.textContent = playerTwo.name;
+    }
+  }
+}
+
+Main.game();
